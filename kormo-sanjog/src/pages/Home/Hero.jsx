1e43c7bc45d2fc66f48340237d1005ebdfd17920
programmerNizam <mdnizam12109@gmail.com>
2024-04-30 20:23:30 +0600

    [Added] Home page hero section content

--- kormo-sanjog/src/pages/Home/Hero.jsx
+++ kormo-sanjog/src/pages/Home/Hero.jsx
@@ -1,20 +1,25 @@
+import heroImg from "../../assets/hero-img.png";
 export default function Hero() {
   return (
-    <div className="">
-      <div>
-        <h1>To Choose Right Jobs.</h1>
-        <p>
-          2400 Peoples are daily search in this portal, 100 user added job
-          portal!
-        </p>
+    <div className="hero py-[70px] pl-[8%]">
+      <div className="grid grid-cols-2 items-center">
         <div>
-          <form action="">
-            <input type="text" />
-            <button type="submit">Search</button>
-          </form>
+          <h1>To Choose Right Jobs.</h1>
+          <p>
+            2400 Peoples are daily search in this portal, 100 user added job
+            portal!
+          </p>
+          <div>
+            <form action="">
+              <input type="text" />
+              <button type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+        <div>
+          <img src={heroImg} alt="hero_img" />
         </div>
       </div>
-      <div></div>
     </div>
   );
 }
