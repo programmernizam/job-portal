cc7285003e4e1a024f6ebd2683bece9787953a00
programmerNizam <mdnizam12109@gmail.com>
2024-04-30 20:13:14 +0600

    [Create] Hero section

---
+++ kormo-sanjog/src/pages/Home/Hero.jsx
@@ -0,0 +1,20 @@
+export default function Hero() {
+  return (
+    <div className="">
+      <div>
+        <h1>To Choose Right Jobs.</h1>
+        <p>
+          2400 Peoples are daily search in this portal, 100 user added job
+          portal!
+        </p>
+        <div>
+          <form action="">
+            <input type="text" />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
