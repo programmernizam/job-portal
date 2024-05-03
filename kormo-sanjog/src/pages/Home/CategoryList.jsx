17e92a86dc394ff300039d1199506d6b0589120b
programmerNizam <mdnizam12109@gmail.com>
2024-05-03 21:52:59 +0600

    [Created] CategoryList Title section

--- kormo-sanjog/src/pages/Home/CategoryList.jsx
+++ kormo-sanjog/src/pages/Home/CategoryList.jsx
@@ -1,5 +1,16 @@
 export default function CategoryList() {
   return (
-    <div>CategoryList</div>
-  )
+    <div className="container pb-[120px] px-2">
+      <div>
+        <h2 className="text-[40px] leading-[48px] font-bold font-exo mb-5">
+          Popular <span className="text-primary">Category</span> List
+        </h2>
+        <p
+          className="text-[18px] leading-[26px] text-[#595959] font-work"
+        >
+          To choose your trending job dream & to make future bright.
+        </p>
+      </div>
+    </div>
+  );
 }
