35638599fd081783254fab41b09a45706745d7ff
programmerNizam <mdnizam12109@gmail.com>
2024-05-17 11:18:02 +0600

    [Added] Featured Jobs Title section

--- kormo-sanjog/src/pages/Home/FeaturedJobs.jsx
+++ kormo-sanjog/src/pages/Home/FeaturedJobs.jsx
@@ -1,15 +1,29 @@
+import { BsArrowRight } from "react-icons/bs";
+
 export default function FeaturedJobs() {
   return (
-    <section className="container">
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <div>
-          <h2>Featured Job List</h2>
-          <p>To choose your trending job dream & to make future bright.</p>
-        </div>
-        <div>
-          <button>
-            <a href="#">Explore More</a>
-          </button>
+    <section className="py-[90px] bg-[#F8F8F8]">
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2 items-end">
+          <div>
+            <h2 className="text-[40px] leading-[48px] font-bold mb-5">
+              <span className="text-primary">Featured</span> Job List
+            </h2>
+            <p className="text-[18px] leading-[26px] text-[#595959]">
+              To choose your trending job dream & to make future bright.
+            </p>
+          </div>
+          <div>
+            <a
+              href="#"
+              className="flex items-center justify-end gap-[3px] text-[17px] leading-[17px] font-medium transition duration-300 hover:text-primary font-exo"
+            >
+              Explore More
+              <span className="ml-5">
+                <BsArrowRight />
+              </span>
+            </a>
+          </div>
         </div>
       </div>
     </section>
